c5a31f3602ee0a8b18f2b354a47f8f3fad00cdd5
Victor Hugo Porfirio <victorhugo.svm@hotmail.com.br>
2026-01-20 15:49:50 -0300

    feat: add authentication middleware for secure file uploads

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import { errorHandling } from "./middlewares/error-handling.js";
 import { routes } from "./routes/index.js";
 import cors from "cors";
 import rateLimit from "express-rate-limit";
+import upload from "./configs/upload.js";
+import { ensureAuthenticated } from "./middlewares/ensure-authenticated.js";
 
 const app = express();
 
@@ -16,6 +18,9 @@ const limiter = rateLimit({
 app.use(cors());
 app.use(limiter);
 app.use(express.json());
+
+app.use("/uploads", ensureAuthenticated,express.static(upload.UPLOADS_FOLDER));
+
 app.use(routes);
 app.use(errorHandling);
 
